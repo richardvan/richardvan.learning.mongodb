344482ebe8009d2ae35de25924013fb9da43345d
Richard Van <richardvan@Richards-Macintosh.local>
2016-04-10 22:46:29 -0700

    commented out mongodb stuff, run index.js for require basics

--- index.js
+++ index.js
@@ -1,29 +1,53 @@
 var mongodb = require('mongodb');
+var fn =require('./myfile.js');
+fn();
 
-var uri = 'mongodb://localhost:27017/example';
-mongodb.MongoClient.connect(uri, function (error,db) {
-  if (error) {
-    console.log(error);
-    process.exit(1);
-  }
+var otherFn = require('./test').other;
+otherFn();
 
-  db.collection('sample').insert({ x: 1 }, function(error, result) {
-    if (error) {
-      console.log(error);
-      process.exit(1);
-    }
-
-  db.collection('sample').find().toArray(function(error, docs){
-    if (error){
-       console.log(error);
-       process.exit(1);
-    }
-
-    console.log('Found docs:');
-    docs.forEach(function(doc) {
-      console.log(JSON.stringify(doc));
-    });
-    process.exit(0);
-    });
-  });
-});
+//var uri = 'mongodb://localhost:27017/example';
+//mongodb.MongoClient.connect(uri, function (error,db) {
+//  if (error) {
+//    console.log(error);
+//    process.exit(1);
+//  }
+//  
+//  var doc = {
+//    title: 'Jaws',
+//    year: 1975,
+//    director: 'Steven Spielberg',
+//    rating: 'PG',
+//    ratings: {
+//      critics: 80,
+//      audience: 97
+//    },
+//    screenplay: ['Peter Benchley', 'Carl Gotlieb']
+//  };
+//  
+//  
+//
+//  db.collection('movies').insert(doc, function(error, result) {
+//    if (error) {
+//      console.log(error);
+//      process.exit(1);
+//    }
+//    
+//  var query1 = { year: 1975, rating: 'PG' };
+//  var query2 = { screenplay: "Peter Benchley" };
+//  var query3 = { 'ratings.audience': { '$gte': 99} };
+//    
+////  db.collection('movies').find(query1).toArray(function(error, docs){
+//  db.collection('movies').find(query3).toArray(function(error, docs){
+//    if (error){
+//       console.log(error);
+//       process.exit(1);
+//    }
+//
+//    console.log('Found docs:');
+//    docs.forEach(function(doc) {
+//      console.log(JSON.stringify(doc));
+//    });
+//    process.exit(0);
+//    });
+//  });
+//});
